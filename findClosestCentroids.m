eaef3ac9c5ff78a3274ec95134f5967e7a06745e
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-08-23 15:02:18 +0300

    closest centroids

--- findClosestCentroids.m
+++ findClosestCentroids.m
@@ -20,6 +20,12 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
+for i = 1:size(X,1)
+	for j = 1:size(centroids,1)
+		dist(j) = sum((X(i,:) - centroids(j,:)).^2);
+	end
+	[Y, idx(i)] = min(dist);
+end
 % =============================================================
 end
 
